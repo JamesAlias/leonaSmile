851245c7bd20b039a3bfa9e2056c23118b716eb4
Robert Baruck <robert.baruck@gmail.com>
2015-06-25 09:31:59 +0200

    Update progress on visuals
    dropPos of notes completed
    start creating drops

--- js/visual.js
+++ js/visual.js
@@ -2,7 +2,16 @@
  * init canvas
  */
 var canvas = document.getElementById('canvas'),
-    cancasCtx = canvas.getContext('2d');
+    cancasCtx = canvas.getContext('2d')
+    drops = [];
 
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
+
+for (var i = 0; i < notes.length; i++) {
+	drops[i] = {};
+
+	drops[i].Pos = {};
+	drops[i].Pos.x = notes[i].dropPos[0];
+	drops[i].Pos.y = notes[i].dropPos[1];
+};
